91056b67a05d35f8a35dcac6125fe726bb79dd80
JiwonKim <jiwonkim188@gmail.com>
2021-05-31 21:55:27 +0900

    Create 13-roman-to-integer.cpp

---
+++ cpp/13-roman-to-integer.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        int ans = 0;
+        
+        for(int i = 0; i < s.size(); i++){
+            if(s[i] == 'I'){
+                if(i < s.size()-1 && (s[i+1] == 'V' || s[i+1] == 'X'))
+                    ans -= 1;
+                else
+                    ans += 1;
+            }
+            else if(s[i] == 'X'){
+                if(i < s.size()-1 && (s[i+1] == 'L' || s[i+1] == 'C'))
+                    ans -= 10;
+                else
+                    ans += 10;
+            }
+            else if(s[i] == 'C'){
+                if(i < s.size()-1 && (s[i+1] == 'D' || s[i+1] == 'M'))
+                    ans -= 100;
+                else
+                    ans += 100;
+            }
+            else if(s[i] == 'V')
+                ans += 5;
+            else if(s[i] == 'L')
+                ans += 50;
+            else if(s[i] == 'D')
+                ans += 500;
+            else if(s[i] == 'M')
+                ans += 1000;
+        }
+        
+        return ans;
+    }
+};
