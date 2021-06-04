a7e2bf620950fb41a43bf747c5963b932427fa9a
JiwonKim <jiwonkim652@gmail.com>
2021-06-04 20:34:58 +0900

    Update 15-3sum.cpp to process duplicates

--- cpp/15-3sum.cpp
+++ cpp/15-3sum.cpp
@@ -7,15 +7,16 @@ public:
         vector<vector<int>> ans;
         sort(nums.begin(), nums.end());
 
-        for(int i = 0; i < nums.size() - 2; i++){
-            for(int j = i + 1; j < nums.size() - 1; j++){
+        for(int i = 0; i < nums.size() - 2;){
+            for(int j = i + 1; j < nums.size() - 1;){          
                 auto it = find(nums.begin() + j + 1, nums.end(), (nums[i] + nums[j]) * -1);
                 if(it != nums.end()){
                     vector<int> triplet = {nums[i], nums[j], nums[it - nums.begin()]};
-                    if(find(ans.begin(), ans.end(), triplet) == ans.end())
-                        ans.push_back(triplet);
+                    ans.push_back(triplet);
                 }
+                while(j < nums.size() - 1 && nums[++j] == nums[j-1]);
             }
+            while(i < nums.size() - 2 && nums[++i] == nums[i-1]);
         }
         
         return ans;
