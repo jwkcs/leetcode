41e7b7a05eae2d328390f71ddb45b9169eb7a24a
JiwonKim <jiwonkim188@gmail.com>
2021-06-06 23:56:33 +0900

    Update 20-valid-parentheses.cpp

--- cpp/20-valid-parentheses.cpp
+++ cpp/20-valid-parentheses.cpp
@@ -1,23 +1,20 @@
 class Solution {
 public:
     bool isValid(string s) {
-        if(s.size() % 2 == 1)
-            return false;
-        if(s[0] == ')' || s[0] == ']' || s[0] == '}')
-            return false;
-        
         unordered_map<char, char> brackets = {{'(', ')'},
                                               {'[', ']'},
                                               {'{', '}'}};
-        int j = 0;
-        string s2 = "";
+        stack<char> s2;
         
         for(int i = 0; i < s.size(); i++)
             if(s[i] == '(' || s[i] == '[' || s[i] == '{')
-                s2[j++] = brackets[s[i]];
-            else if(--j < 0 || s[i] != s2[j])
-                return false;
+                s2.push(brackets[s[i]]);
+            else {
+                if(s2.empty() || s[i] != s2.top())
+                    return false;
+                s2.pop();
+            }
         
-        return j == 0 ? true : false;
+        return s2.empty();
     }
 };
