f8c75a074004e1a6bccfd1a1152c25d257c567d6
JiwonKim <jiwonkim652@gmail.com>
2021-06-13 14:55:56 +0900

    Update 15-3sum.cpp

--- cpp/15-3sum.cpp
+++ cpp/15-3sum.cpp
@@ -3,20 +3,32 @@ public:
     vector<vector<int>> threeSum(vector<int>& nums) {
         if(nums.size() < 3)
             return {};
-
+        
+        int L = 0;
+        int R = 0;
+        int sum = 0;
         vector<vector<int>> ans;
+        
         sort(nums.begin(), nums.end());
-
+        
         for(int i = 0; i < nums.size() - 2;){
-            for(int j = i + 1; j < nums.size() - 1;){          
-                auto it = find(nums.begin() + j + 1, nums.end(), (nums[i] + nums[j]) * -1);
-                if(it != nums.end()){
-                    vector<int> triplet = {nums[i], nums[j], nums[it - nums.begin()]};
-                    ans.push_back(triplet);
+            L = i + 1;
+            R = nums.size() - 1;
+            
+            while(L < R){
+                sum = nums[i] + nums[L] + nums[R];
+                
+                if(sum > 0)
+                    while(L < --R && nums[R] == nums[R+1]);
+                else if(sum < 0)
+                    while(++L < R && nums[L] == nums[L-1]);
+                else{
+                    ans.push_back(vector<int>{nums[i], nums[L], nums[R]});
+                    while(L < --R && nums[R] == nums[R+1]);
+                    while(++L < R && nums[L] == nums[L-1]);
                 }
-                while(j < nums.size() - 1 && nums[++j] == nums[j-1]);
             }
-            while(i < nums.size() - 2 && nums[++i] == nums[i-1]);
+            while(++i < nums.size() - 2 && nums[i] == nums[i-1]);
         }
         
         return ans;
