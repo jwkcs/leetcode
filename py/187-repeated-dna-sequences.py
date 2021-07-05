62c8b73f723687f893d471aea0109edcb966d495
JiwonKim <jiwonkim188@gmail.com>
2021-07-05 17:25:26 +0900

    Update 187-repeated-dna-sequences.py

--- py/187-repeated-dna-sequences.py
+++ py/187-repeated-dna-sequences.py
@@ -1,11 +1,12 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
         d = dict()
+        ret = dict()
         for i in range(len(s)):
             if i+10 > len(s):
                 break
             if s[i:i+10] not in d:
                 d[s[i:i+10]] = 1
             else:
-                d[s[i:i+10]] += 1
-        return list({key for key, value in d.items() if value >= 2})
+                ret[s[i:i+10]] = 1
+        return list(ret.keys())
