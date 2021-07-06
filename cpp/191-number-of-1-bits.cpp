3170a2d1354a94922efeaf5f040a551a7499b661
JiwonKim <jiwonkim188@gmail.com>
2021-07-06 12:12:37 +0900

    Create 191-number-of-1-bits.cpp

---
+++ cpp/191-number-of-1-bits.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int ret = 0;
+        
+        while(n > 0){
+            if(n % 2 == 1)
+                ret++;
+            n = n >> 1;
+        }
+        
+        return ret;
+    }
+};
