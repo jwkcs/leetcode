cca4e74bc27959797878b98d5219d241da3c2c58
JiwonKim <jiwonkim652@gmail.com>
2021-07-07 14:09:57 +0900

    Update 191-number-of-1-bits.cpp

--- cpp/191-number-of-1-bits.cpp
+++ cpp/191-number-of-1-bits.cpp
@@ -3,10 +3,9 @@ public:
     int hammingWeight(uint32_t n) {
         int ret = 0;
         
-        while(n > 0){
-            if(n % 2 == 1)
-                ret++;
-            n = n >> 1;
+        while(n){
+            n &= n-1;
+            ret++;
         }
         
         return ret;
